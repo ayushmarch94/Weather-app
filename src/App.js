a580ef7bd2d13619e5008a854428df8e4d8b18f8
ayushmarch94 <ayushmarch94@gmail.com>
2023-07-28 06:46:00 +0530

    added heading and minor style changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [city,setCity] = useState('----');
+  const [temperature, setTemperature] = useState('');
+  useEffect(() => {
+    if (city !== '') {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ad8300df27420798bcf5004c184de162`;
+
+      // Fetch temperature from the API
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const temp = (data.main.temp-273.15);
+          setTemperature(temp.toFixed(2));
+        })
+        .catch(error => {
+          console.error('Error fetching temperature:', error);
+        });
+    }
+  }, [city]);
+  function Finder (event){
+    setCity(event.target.value);
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="heading">
+        
+        <p><img src={require('./imgaes/weather.png')} width={30} />Current Weather Forcaste</p>
+      </div>
+      <div id="page">
+        <div id="main">
+          <div id="navbar">
+            <input type='search' placeholder=' Location' onChange={Finder} />
+          </div>
+          <div id="mid">
+            <div id="logo">
+              <img src={require('./imgaes/map.png')} width="50px" />
+            </div>
+            <div id="location">
+              {city}
+            </div>
+          </div>
+          <div id="lower">
+            <div id="animation">
+              <div id="lowermid">
+                <div id="temprature">
+                  {temperature}
+                </div>
+                <div id="unit">
+                  °
+                </div>
+              </div>
+              <div id="lowerlower">
+                Current Temprature
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
   );
 }
 
