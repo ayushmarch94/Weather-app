b44c517a89d76c012b9ee46305ac4a93ad11a754
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-08-22 17:01:26 +0530

    Added component for input field

--- src/App.js
+++ src/App.js
@@ -1,55 +1,51 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Input from "./component/Input";
 
 function App() {
-  const [city,setCity] = useState('Delhi');
-  const [temperature, setTemperature] = useState('');
+  const [city, setCity] = useState("Delhi");
+  const [temperature, setTemperature] = useState("");
   useEffect(() => {
-    if (city !== '') {
+    if (city !== "") {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ad8300df27420798bcf5004c184de162`;
 
       // Fetch temperature from the API
       fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          const temp = (data.main.temp-273.15);
+        .then((response) => response.json())
+        .then((data) => {
+          const temp = data.main.temp - 273.15;
           setTemperature(temp.toFixed(2));
         })
-        .catch(error => {
-          console.error('Error fetching temperature:', error);
+        .catch((error) => {
+          console.error("Error fetching temperature:", error);
         });
     }
   }, [city]);
-  function Finder (event){
+  function Finder(event) {
     setCity(event.target.value);
-    
   }
 
   return (
     <>
       <div id="heading">
-        
-        <p><img src={require('./images/weather.png')} width={30} />Current Weather Forcaste</p>
+        <p>
+          <img src={require("./images/weather.png")} width={30} />
+          Current Weather Forcaste
+        </p>
       </div>
       <div id="page">
         <div id="main">
-          <div id="navbar">
-            <input type='search' placeholder={' Location : Random '} onChange={Finder} />
-          </div>
-          <div id="mid">
-          </div>
+          <Input Finder={Finder} />
+          <div id="mid"></div>
           <div id="lower">
             <div id="animation">
               <div id="lowermid">
-                <div id="temprature">
-                  {temperature}
-                </div>
-                <div id="unit">
-                  °
-                </div>
+                <div id="temprature">{temperature}</div>
+                <div id="unit">°</div>
               </div>
               <div id="lowerlower">
-                Current Temprature<br /> 
+                Current Temprature
+                <br />
                 {city}
               </div>
             </div>
---
+++ src/component/Input.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../component/Input";
+
+
+export default function Input({ Finder }) {
+  return (
+    <div id="navbar">
+      <input
+        type="search"
+        placeholder={" Location : Random "}
+        onChange={Finder}
+      />
+    </div>
+  );
+}
