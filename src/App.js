9e0b59a21ec140fb0c44bde12a495c5a9e7c44d5
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-09-24 15:40:50 +0530

    updated UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import "./App.css";;
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 export default function App() {
+  const [location, setLocation] = useState("");
+  const [temperature, setTemperature] = useState("");
+
+  const handleInputChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchWeather();
+  };
+
+  const fetchWeather = () => {
+    if (location) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7c308b119937245fada5651b91c31922`)
+        .then((response) => response.json())
+        .then((data) => setTemperature(data.main.temp));
+    }
+  };
+
   return (
-    <div id='container'>
-          <div id='main'>
-            <form id='form'>
-              <input type='text' placeholder='Enter Location' />
-              <button type='submit'>Search</button>
-            </form>
-            <div id="display">
-              
-            </div>
-          </div>
+    <div id="container">
+      <div id="main">
+        <p>Weather Forecast </p>
+        <form id="form" onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter Location"
+            onChange={handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <div id="display">
+          {location && temperature && (
+            <p>{(temperature - 273).toFixed(2)}°C</p>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
